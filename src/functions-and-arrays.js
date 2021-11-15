cfd9d4a14f2c259a9d112ab852b1cc0c7d3713af
Sebas Corral <sebascorralbrito@gmail.com>
2021-11-15 18:35:23 +0100

    add

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,45 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if(words.length === 0){
+    return null;
+  }
+
+  let longestWord = "";
+  for (let index = 0; index < words.length; index++) {
+    const word = words[index];
+    if (words.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+findLongestWord(words);
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let resultNumbers = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    resultNumbers += numbers[i];
+  }
+  return resultNumbers;
+}
 
 
 
